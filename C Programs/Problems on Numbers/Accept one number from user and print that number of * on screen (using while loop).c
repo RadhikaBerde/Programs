5e4dbca7ab832201e1fe7d5d4134445a9454b7b9
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-11-26 23:05:45 +0530

    Create Accept one number from user and print that number of * on screen (using while loop).c

---
+++ C Programs/Problems on Numbers/Accept one number from user and print that number of * on screen (using while loop).c
@@ -0,0 +1,30 @@
+Accept one number from user and print that number of * on screen (using while loop).
+
+#include<stdio.h>
+
+void Accept(int iNo)
+    {
+
+        while(iNo>0)
+        {
+           printf("*");
+           iNo--;
+
+
+        }
+
+    }
+
+int main()
+{
+
+    int iValue=0;
+
+    printf("Enter any no.: ");
+    scanf("%d",&iValue);
+    Accept(iValue);
+
+    return 0;
+
+}
+
