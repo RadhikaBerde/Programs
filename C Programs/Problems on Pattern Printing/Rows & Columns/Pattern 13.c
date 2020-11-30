acc1a3a04b7a4348166e8a736e81e00134b87b81
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-11-30 21:33:53 +0530

    Create Pattern 13.c

---
+++ C Programs/Problems on Pattern Printing/Rows & Columns/Pattern 13.c
@@ -0,0 +1,63 @@
+/* Accept number of rows and number of columns from user and display below pattern.
+
+Input : iRow = 5 iCol = 5
+Output :
+ a b c d e
+ 1 2 3 4 5
+ a b c d e
+ 1 2 3 4 5
+ a b c d e
+
+ */
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i=0;
+    int j=0;
+    int iNo=1;
+    char ch='/0';
+
+    if(iRow<0)
+    {
+        iRow=-iRow;
+    }
+    if(iCol<0)
+    {
+        iCol=-iCol;
+    }
+
+    for(i=1;i<=iRow;i++)
+     {
+        ch='a';
+        iNo=1;
+
+        for(j=1;j<=iCol;j++)
+        {
+            if((i%2)==0)
+              {
+                 printf("%d\t",iNo);
+                 iNo++;
+              }
+            else
+             {
+                 printf("%c\t",ch);
+                 ch++;
+             }
+         }
+            printf("%\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
