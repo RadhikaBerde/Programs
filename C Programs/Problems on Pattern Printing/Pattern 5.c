a3f8c7d72589b2364f1ad34db48aa01827ac379f
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-11-30 19:41:08 +0530

    Create Pattern 5.c

---
+++ C Programs/Problems on Pattern Printing/Pattern 5.c
@@ -0,0 +1,36 @@
+/* Accept number from user and display below pattern.
+
+Input : 4
+Output : # 1 * # 2 * # 3 * # 4 *
+
+*/
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt=0;
+
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+
+    for(iCnt=1;iCnt<=iNo;iCnt++)
+    {
+        printf(" #%d*\t", iCnt);
+    }
+}
+
+int main()
+{
+ int iValue = 0;
+
+ printf("Enter number of elements: ");
+ scanf("%d",&iValue);
+
+ Pattern(iValue);
+
+ return 0;
+}
+
