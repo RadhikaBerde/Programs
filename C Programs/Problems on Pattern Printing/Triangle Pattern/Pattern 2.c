9a306af42a3639d7dfe05a175e787e13b836b337
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-11-30 23:03:19 +0530

    Update Pattern 2.c

--- C Programs/Problems on Pattern Printing/Triangle Pattern/Pattern 2.c
+++ C Programs/Problems on Pattern Printing/Triangle Pattern/Pattern 2.c
@@ -8,3 +8,53 @@ Output :
  *
 
  */
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i=0;
+    int j=0;
+
+    if(iRow<0)
+    {
+        iRow=-iRow;
+    }
+    if(iCol<0)
+    {
+        iRow=-iCol;
+    }
+    if(iRow!=iCol)
+    {
+        printf("Invalid Input");
+        return;
+    }
+        for(i=1;i<=iRow;i++)
+          {
+             for(j=1;j<=iCol;j++)
+                {
+                    if(j<=5-i)
+                        {
+                            printf("*\t");
+                        }
+                    else
+                        {
+                            printf(" \t");
+                        }
+                }
+                        printf("\n");
+
+            }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
+
