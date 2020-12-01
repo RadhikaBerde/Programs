aae94f8a9032e7e68cc0a72dfabd9ad45e9cfa82
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-01 12:35:47 +0530

    Create Display all such elements which are multiples of 11.c

---
+++ C Programs/Problems on Array/Display all such elements which are multiples of 11.c
@@ -0,0 +1,70 @@
+/* Accept N numbers from user and display all such elements which are multiples of 11.
+
+Input : N : 6
+Elements : 85 66 3 55 93 88
+Output : 66 55 88
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display (int arr[],int iLength)
+{
+  int iCnt=0;
+
+  if(arr == NULL)
+    {
+        printf("Invalid address ");
+        return 0;
+    }
+ if(iLength<=0)
+    {
+        printf("Invalid size");
+        return 0;
+    }
+ printf("Multiples of 11 are: ");
+ for(iCnt=0; iCnt<iLength;iCnt++)
+  {
+    if(arr[iCnt]%11==0)
+      {
+          printf("%d\t",arr[iCnt]);
+      }
+  }
+}
+
+int main()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements: \n");
+    scanf("%d",&iSize);
+
+    if(iSize==0)
+    {
+        printf("Invalid size");
+        return -1;
+    }
+
+    p=(int *)malloc(iSize * 4);
+
+    if(p==NULL)
+     {
+        printf("unable to allocate memory ");
+        return -1;
+    }
+
+    printf("Enter the elements : \n");
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+    Display(p,iSize);
+
+    free(p);
+
+    return 0;
+}
+
