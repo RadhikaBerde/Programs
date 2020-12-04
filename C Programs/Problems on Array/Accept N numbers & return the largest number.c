0a26cc9be939a1548eaf28e5d5f6070215686704
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-04 16:28:30 +0530

    Create Accept N numbers & return the largest number.c

---
+++ C Programs/Problems on Array/Accept N numbers & return the largest number.c
@@ -0,0 +1,75 @@
+/* Accept N numbers from user and return the largest number.
+
+Input : N : 6
+Elements : 85 66 3 66 93 88
+Output : 93
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Largest(int arr[],int iSize)
+{
+    int iMax=0;
+    int iCnt=0;
+
+    if(arr==NULL)
+    {
+        printf("Invalid Address");
+        return 0;
+    }
+    if(iSize<0)
+    {
+        printf("Invalid Size");
+        return 0;
+    }
+    iMax=arr[0];
+    for(iCnt=0;iCnt<iSize;iCnt++)
+    {
+        if(arr[iCnt]>iMax)
+        {
+            iMax=arr[iCnt];
+        }
+    }
+    return iMax;
+}
+int main()
+{
+    int *p=NULL;
+    int iSize=0;
+    int iRet=0;
+    int i=0;
+
+    printf("Enter size: ");
+    scanf("%d",&iSize);
+
+    if(iSize<0)
+    {
+        printf("Invalid Size");
+        return -1;
+    }
+
+    p=(int*)malloc(sizeof(int)*iSize);
+
+    if(p==NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter the elements: ");
+    for(i=0;i<iSize;i++)
+    {
+        scanf("%d",&p[i]);
+    }
+
+     iRet=Largest(p,iSize);
+
+     printf("Largest no is: %d",iRet);
+
+     free(p);
+
+     return 0;
+}
+
