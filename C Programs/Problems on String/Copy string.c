e4b331f65d9d16748b0fa47cf918b30f34902a4f
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-06 12:33:00 +0530

    Update Copy string.c

--- C Programs/Problems on String/Copy string.c
+++ C Programs/Problems on String/Copy string.c
@@ -1,22 +1,19 @@
-/* Write a program which accept string from user and copy the contents of that string into another string. (Implement strncpy() function)
+/* 1.Write a program which accept string from user and copy the contents of that string into another string. (Implement strcpy() function)
 
 Input : “Marvellous Multi OS”
-        10
-Output : “Marvellous
+Output : “Marvellous Multi OS” in another string
 
-Note : If third parameter is greater than the size of source string then copy whole string into destination.
 */
 
 #include<stdio.h>
 
-void StrlenX(char *str, char *dest,int iCnt)
+void StrlenX(char *str, char *dest)
 {
-   while((*str!= '\0')&&(iCnt!=0))
+   while(*str!= '\0')
     {
-       *dest = *str;
-        dest++;
-        str++;
-        iCnt--;
+     *dest = *str;
+      str++;
+      dest++;
     }
     *dest='\0';
 }
@@ -25,15 +22,11 @@ int main()
 {
     char arr[30];
     char brr[30] ;      // Static memory allocation of array
-    int iCnt=0;
 
     printf("Enter the string: ");  //scanf("%s",arr);
     scanf("%[^'\n']s",arr);     // Accept the input till user enters enter key
 
-    printf("Enter the no: ");
-    scanf(" %d",&iCnt);
-
-    StrlenX(arr,brr,iCnt);           // StrlenX(100) // 10
+    StrlenX(arr,brr);           // StrlenX(100) // 10
 
     printf("Copied string is:  %s ",brr);
 
