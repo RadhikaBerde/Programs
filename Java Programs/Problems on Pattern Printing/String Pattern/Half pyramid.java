44ab25c277bce083ad1d07ba065719bb372066a9
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-11 23:24:58 +0530

    Create Half pyramid.java

---
+++ Java Programs/Problems on Pattern Printing/String Pattern/Half pyramid.java
@@ -0,0 +1,53 @@
+/* Write Java program which accept String from user and display below pattern.
+
+Input : Hello
+Output : 
+ H
+ H e
+ H e l
+ H e l l
+ H e l l o 
+
+*/
+
+import java.lang.*;
+import java.util.*;
+
+class Pattern
+{
+    void PrintPattern(String str)
+     {
+       int i=0;
+       int j=0;
+       
+       char arr[]=str.toCharArray();
+       int size=arr.length;
+             
+       for(i=1;i<=size;i++)
+        {
+            int k=0;
+            
+           for(j=1;j<=i;j++)
+            {
+               System.out.print(arr[k]+"\t");
+               k++;
+           }
+           
+            System.out.println();                    
+       }             
+   }    
+}
+
+class Demo
+{
+    public static void main(String arg[])
+     {
+        Scanner sobj=new Scanner(System.in);
+        Pattern pobj=new Pattern();
+        
+        System.out.println("Enter string:  ");
+        String str=sobj.nextLine();
+                
+        pobj.PrintPattern(str);
+    }
+}
