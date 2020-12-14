509459d0894e0f01371d0498852c7449abc878d6
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-14 15:50:36 +0530

    Create Toggle 7th bit of no. Return modified no.c

---
+++ C Programs/Problems on Bitwise Operators/Toggle 7th bit of no. Return modified no.c
@@ -0,0 +1,32 @@
+/* Write a program which accept one number from user and toggle 7th and 10th bit of that number. Return modified number.
+Input : 137
+Output : 713
+
+*/
+
+#include<stdio.h>
+int ToggleBit(int iNo)
+{
+    int iMask=0X00000240;
+    int iResult=0;
+
+    if(iNo<0)
+     {
+        iNo=-iNo;
+     }
+    iResult=iMask^iNo;
+    return iResult;
+}
+
+int main()
+{
+    int iNo=0;
+    int iAns=0;
+
+    printf("Enter number: ");
+    scanf("%d",&iNo);
+
+    iAns=ToggleBit(iNo);
+
+    printf("%d",iAns);
+}
