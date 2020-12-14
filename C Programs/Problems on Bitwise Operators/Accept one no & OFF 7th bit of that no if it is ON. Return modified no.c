b7ecda79cf1b4ba8a0fc75c538ee403abd167fa3
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-14 15:43:41 +0530

    Create Accept one no & OFF 7th bit of that no if it is ON. Return modified no.c

---
+++ C Programs/Problems on Bitwise Operators/Accept one no & OFF 7th bit of that no if it is ON. Return modified no.c
@@ -0,0 +1,32 @@
+/*1.Write a program which accept one number from user and off 7th bit of that number if it is on. Return modified number.
+
+Input : 79
+Output : 15
+
+*/
+
+#include<stdio.h>
+
+int OffBit(int iNo)
+{
+    int iMask=0X00000040;
+    int iResult=0;
+
+    iResult=iMask^iNo;
+
+    return iResult;
+}
+
+int main()
+{
+    int iNo=0;
+    int iAns=0;
+
+    printf("Enter number: ");
+    scanf("%d",&iNo);
+
+    iAns=OffBit(iNo);
+
+    printf("%d",iAns);
+    return 0;
+}
