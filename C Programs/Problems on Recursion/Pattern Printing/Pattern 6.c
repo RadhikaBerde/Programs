3b19b9fd21b4df285bda62bd02a818b5b548662d
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-14 23:57:30 +0530

    Create Pattern 6.c

---
+++ C Programs/Problems on Recursion/Pattern Printing/Pattern 6.c
@@ -0,0 +1,31 @@
+/* Write a recursive program which display below pattern.
+
+Input : 5
+Output : * * * * *
+
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+
+    if(iNo>0)
+    {
+        printf("*\t");
+        iNo--;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue=0;
+
+    printf("Enter number: \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
