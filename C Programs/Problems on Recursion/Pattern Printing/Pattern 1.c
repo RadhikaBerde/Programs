67dac886af87677e44e7b4fa909df00cfae48373
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2020-12-14 23:49:49 +0530

    Create Pattern 1.c

---
+++ C Programs/Problems on Recursion/Pattern Printing/Pattern 1.c
@@ -0,0 +1,24 @@
+/*1. Write a recursive program which display below pattern.
+
+     Output : * * * * *
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+    static int i=1;
+    
+    if(i<=5)
+    {
+        printf("*\t");
+        i++;
+        Display(i);
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
