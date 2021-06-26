5960993f5c8131a444b7ffccbc2b88af6d75a999
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2021-06-26 14:39:26 +0530

    Create 5.Display 10 to 1 on screen.py

---
+++ Python Programs/Assignment No 1/5.Display 10 to 1 on screen.py
@@ -0,0 +1,16 @@
+"""5.Write a program which display 10 to 1 on screen.
+
+Output : 10 9 8 7 6 5 4 3 2 1 
+
+"""
+def PrintNo():
+    
+	for i in range(10,0,-1):
+		print(i)
+	
+def main():
+	PrintNo()
+    
+if __name__=="__main__":
+	main()
+	
