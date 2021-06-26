5d35b3c5817919e83b58d14285103065494b5754
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2021-06-26 14:38:11 +0530

    Create 4.Display 5 times Marvellous on screen.py

---
+++ Python Programs/Assignment No 1/4.Display 5 times Marvellous on screen.py
@@ -0,0 +1,19 @@
+"""4.Write a program which display 5 times Marvellous on screen.
+ Output : 
+ Marvellous
+ Marvellous
+ Marvellous
+ Marvellous
+ Marvellous 
+ """
+ 
+def Print():
+    i=0
+    for i in range (5):
+        print("Marvellous")
+        
+def main():
+    Print()
+    
+if __name__=="__main__":
+    main()
