a1469e6ad8c05ccdae40997c7476fbadcb012064
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2021-12-04 19:01:32 +0530

    Create if_else.ts

---
+++ TypeScript/Part 3/if_else.ts
@@ -0,0 +1,21 @@
+console.log("Marvellous Infosystems : if else construct"); 
+function ChkEvenOdd(num:number) 
+{ 
+ if (num % 2 == 0) 
+ { 
+ console.log("Even"); 
+ } 
+ else 
+ { 
+ console.log("Odd"); 
+ } 
+ } 
+var no = 12; 
+ChkEvenOdd(no);
+
+/*
+O/P:
+
+Marvellous Infosystems : if else construct
+Even
+*/
