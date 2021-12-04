229958cdef6780c4593e8ac1f7e1158f1525aecb
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2021-12-04 19:33:02 +0530

    Update 4_Check_Prime.ts

--- TypeScript/Assignment No 1/4_Check_Prime.ts
+++ TypeScript/Assignment No 1/4_Check_Prime.ts
@@ -1,1 +1,38 @@
-abc
+/*4. Write a typescript program which contains one function named as ChkPrime. That function should
+accept one number and it should return true if the given number is prime and otherwise return false.
+Input : 11
+Output : It is prime number */
+
+main();
+
+function ChkPrime (iNo : number) : boolean
+{
+	var i : number = 0;
+	var bTemp : boolean = false;
+	for ( i =0 ; i<= iNo/2 ; i++ )
+	{
+		if(iNo%2 == 0)
+		{
+			bTemp = true;
+		}
+	}
+	return bTemp;
+	 
+}
+
+function main()
+{
+	var iNo : number = 11;
+	var bRet : boolean = false;
+
+	bRet=ChkPrime(iNo);
+
+	if(bRet== false )
+	{
+		console.log("It is a Prime no. ");
+	}
+	else 
+	{
+		console.log("It is not a Prime no. ");
+	}
+}
