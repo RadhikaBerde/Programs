34813f1478e86967d1876d0396aa15614faef8a1
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2021-12-05 11:16:03 +0530

    Update 4_Check_Armstrong.ts

--- TypeScript/Assignment No 2/4_Check_Armstrong.ts
+++ TypeScript/Assignment No 2/4_Check_Armstrong.ts
@@ -1,1 +1,33 @@
-Confirm
+/*
+4. Write a typescript program which contains one arrow function named as ChkArmstrong. 
+That function accepts one numbers and check whether number is Armstrong number or not. 
+
+Input : 153 
+Output : It is Armstrong number
+
+*/
+
+
+var ChkArmstrong = ( iNo : number) => {
+	
+	
+	var iTemp : number =iNo;
+	while(iNo>0)
+	{
+		var iTemp=iNo%10;
+		var iAns= iAns+(iTemp*iTemp*iTemp);
+		iNo=iNo/10;
+	}
+	
+	if(iTemp == iAns)
+	{
+		console.log("It is Armstrong no.");
+	}
+	else
+	{
+		console.log("It is not Armstrong no ");
+	}
+	
+}
+
+ChkArmstrong(153);
