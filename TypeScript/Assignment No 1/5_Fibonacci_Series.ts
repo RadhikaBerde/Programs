f5909e4787bcd0e93c931693a3e8f4b2384b21b1
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2021-12-05 10:28:00 +0530

    Update 5_Fibonacci_Series.ts

--- TypeScript/Assignment No 1/5_Fibonacci_Series.ts
+++ TypeScript/Assignment No 1/5_Fibonacci_Series.ts
@@ -1,1 +1,34 @@
-confirm
+/*5. Write a typescript program which contains one function named as Fibonacci. That function accept
+one number from user and print Fibonacci series till that number.
+Input : 21
+Output : 1 1 2 3 5 8 13 21
+*/
+
+main();
+
+function Fibonacci ( iNo : number ) 
+{
+	var iFirst : number = 0;
+	var iSecond : number = 1;
+	var iThird : number = 0;
+	var i : number = 0;
+	
+  console.log(iSecond);//printing 1 
+	for(i=2;iThird<iNo;i++)//loop starts from 2 because 1 are already printed    
+	{    
+	  iThird=iFirst+iSecond;    
+	  console.log(iThird);    
+	  iFirst=iSecond;    
+	  iSecond=iThird;    
+	}  
+	
+}
+
+function main()
+{
+
+	var iNo : number = 21;
+
+	Fibonacci( iNo);
+}
+
